b434546ee2290775dacf7acfd3fff47fc004811a
Doreen Mensah <doreenradiance@gmail.com>
2021-03-12 18:08:11 +0000

    Edited line 2

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, TextInput, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function ProfilePage() {
