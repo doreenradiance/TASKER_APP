79418701de33957f9dcd99c3525bfd029b6f1c94
Doreen Mensah <doreenradiance@gmail.com>
2021-03-12 17:07:47 +0000

    Routed the signupPage

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginPage from './src/screens/LoginPage';
 import StartupPage from './src/screens/StartupPage';
+import SignupPage from './src/screens/SignupPage'
 
 export default function App() {
   return(
     // <StartupPage/>
-    <LoginPage/>
+    // <LoginPage/>
+    <SignupPage/>
   )
 }
