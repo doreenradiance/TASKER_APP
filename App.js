9bca902e84ca1bdb66b1fce0bb31d498db865605
Doreen Mensah <doreenradiance@gmail.com>
2021-03-13 12:45:18 +0000

    Routed the tasksPage

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import LoginPage from './src/screens/LoginPage';
 import StartupPage from './src/screens/StartupPage';
 import SignupPage from './src/screens/SignupPage'
 import ProfilePage from './src/screens/ProfilePage';
+import TasksPage from './src/screens/TasksPage';
 
 export default function App() {
   return(
     // <StartupPage/>
     // <LoginPage/>
     // <SignupPage/>
-    <ProfilePage/>
+    // <ProfilePage/>
+    // <TasksPage/>
+    <TasksPage/>
   )
 }
