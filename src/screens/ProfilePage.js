bbaf5202d751cf33c917b00cc22676ef7e18f615
Doreen Mensah <doreenradiance@gmail.com>
2021-03-22 15:51:17 +0000

    Passed the navigation prop on the account text

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-export default function ProfilePage({navigation}) {
+export default function ProfilePage({ navigation }) {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity onPress={()=>{
+                <TouchableOpacity onPress={() => {
                     navigation.navigate("Startup")
                 }}>
                     <AntDesign name="back" size={24} color="black" style={styles.icon} />
@@ -20,23 +20,31 @@ export default function ProfilePage({navigation}) {
             <Text style={styles.location}>Los Angeles,City</Text>
 
             <View style={styles.info}>
-            <TouchableOpacity onPress={()=>{
-                navigation.navigate('Tasks')
-            }}>
-                <Text style={{ fontSize: 17, marginLeft: 45 }}>Tasks</Text>
+                <TouchableOpacity onPress={() => {
+                    navigation.navigate('Tasks')
+                }}>
+                    <Text style={{ fontSize: 17, marginLeft: 45 }}>Tasks</Text>
                 </TouchableOpacity>
                 <AntDesign name="right" size={24} color="black" style={{ marginLeft: 210 }} />
             </View>
             <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
 
             <View style={styles.info}>
-                <Text style={{ fontSize: 17, marginLeft: 45 }}>Account</Text>
+                <TouchableOpacity onPress={() => {
+                    navigation.navigate('Account')
+                }}>
+                    <Text style={{ fontSize: 17, marginLeft: 45 }}>Account</Text>
+                </TouchableOpacity>
                 <AntDesign name="right" size={24} color="black" style={{ marginLeft: 200 }} />
             </View>
             <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
 
             <View style={styles.info}>
+            <TouchableOpacity onPress={()=>{
+                navigation.navigate('Activity')
+            }}>
                 <Text style={{ fontSize: 17, marginLeft: 45 }}>Activity</Text>
+                </TouchableOpacity>
                 <AntDesign name="right" size={24} color="black" style={{ marginLeft: 200 }} />
             </View>
             <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
