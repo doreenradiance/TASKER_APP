df8a449df055b990c8c47c7ea77c55e494940f21
Doreen Mensah <doreenradiance@gmail.com>
2021-03-22 13:15:05 +0000

    Corrected some codes

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import StartupPage from './src/screens/StartupPage';
 import LoginPage from './src/screens/LoginPage';
 import SignupPage from './src/screens/SignupPage'
@@ -12,37 +11,24 @@ import AccountPage from "./src/screens/AccountPage";
 import ActivityPage from "./src/screens/ActivityPage";
 import WithdrawalPage from "./src/screens/WithdrawalPage";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-
 const Stack = createStackNavigator()
+
 export default function App() {
   return (
-
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screens name="Login" component={LoginPage}/>
-        <Stack.Screens name="Signup" component={SignupPage}/>
-        <Stack.Screens name="Profile" component={ProfilePage}/>
-        <Stack.Screens name="Tasks" component={TasksPage}/>
-        <Stack.Screens name="Detail" component={TaskDetail}/>
-        <Stack.Screens name="Account" component={AccountPage}/>
-        <Stack.Screens name="Activity" component={ActivityPage}/>
-        <Stack.Screens name="Withdraw" component={WithdrawalPage}/>
-    
+        <Stack.Screen name="Startup" component={StartupPage} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen name="Profile" component={ProfilePage} />
+        <Stack.Screen name="Tasks" component={TasksPage} />
+        <Stack.Screen name="Detail" component={TaskDetail} />
+        <Stack.Screen name="Account" component={AccountPage} />
+        <Stack.Screen name="Activity" component={ActivityPage} />
+        <Stack.Screen name="Withdraw" component={WithdrawalPage} />
+
       </Stack.Navigator>
 
     </NavigationContainer>
-
-
-    // <StartupPage />
-    // <LoginPage />
-    // <SignupPage />
-    // <ProfilePage />
-    // <TasksPage />
-    // <TaskDetail />
-    // <AccountPage />
-    // <ActivityPage />
-    // <WithdrawalPage />
   )
 }
