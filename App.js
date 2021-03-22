e6bd221fd5bafee524e63eaccc36b3d7d6571241
Doreen Mensah <doreenradiance@gmail.com>
2021-03-22 12:42:11 +0000

    Created the stacksScreens

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import LoginPage from './src/screens/LoginPage';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import StartupPage from './src/screens/StartupPage';
+import LoginPage from './src/screens/LoginPage';
 import SignupPage from './src/screens/SignupPage'
 import ProfilePage from './src/screens/ProfilePage';
 import TasksPage from './src/screens/TasksPage';
@@ -9,16 +12,37 @@ import AccountPage from "./src/screens/AccountPage";
 import ActivityPage from "./src/screens/ActivityPage";
 import WithdrawalPage from "./src/screens/WithdrawalPage";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 export default function App() {
-  return(
-    // <StartupPage/>
-    // <LoginPage/>
-    // <SignupPage/>
-    // <ProfilePage/>
-    <TasksPage/>
-    // <TaskDetail/>
-    // <AccountPage/>
-    // <ActivityPage/>
-    // <WithdrawalPage/>
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screens name="Login" component={LoginPage}/>
+        <Stack.Screens name="Signup" component={SignupPage}/>
+        <Stack.Screens name="Profile" component={ProfilePage}/>
+        <Stack.Screens name="Tasks" component={TasksPage}/>
+        <Stack.Screens name="Detail" component={TaskDetail}/>
+        <Stack.Screens name="Account" component={AccountPage}/>
+        <Stack.Screens name="Activity" component={ActivityPage}/>
+        <Stack.Screens name="Withdraw" component={WithdrawalPage}/>
+    
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+
+    // <StartupPage />
+    // <LoginPage />
+    // <SignupPage />
+    // <ProfilePage />
+    // <TasksPage />
+    // <TaskDetail />
+    // <AccountPage />
+    // <ActivityPage />
+    // <WithdrawalPage />
   )
 }
