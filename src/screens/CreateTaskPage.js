ad56c9d46fabed881aa85224ee116a36949b5920
Doreen Mensah <doreenradiance@gmail.com>
2021-04-13 10:34:18 +0000

    Put then whole container in a scrollview and a marginTop for each item

--- src/screens/CreateTaskPage.js
+++ src/screens/CreateTaskPage.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, TextInput, View } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { AntDesign } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { createTask } from '../redux/actions/taskActions';
 import { showMessage } from 'react-native-flash-message';
+import { ScrollView } from 'react-native-gesture-handler';
 
 function CreateTaskPage({ navigation, appState, createTask }) {
     const [showDatePicker, setShowDatePicker] = useState(false)
-    const [task , setTask] = useState({
+    const [task, setTask] = useState({
         title: "",
         description: "",
         location: "",
@@ -19,12 +20,12 @@ function CreateTaskPage({ navigation, appState, createTask }) {
 
     const onChange = (val, name) => {
         //change amount value to number
-        if(name === 'amount') {
+        if (name === 'amount') {
             setTask((prev) => ({
                 ...prev,
                 [name]: parseInt(val)
             }))
-        }else {
+        } else {
             setTask(prev => ({
                 ...prev,
                 [name]: val
@@ -38,18 +39,18 @@ function CreateTaskPage({ navigation, appState, createTask }) {
         onChange(date, "date")
     }
 
-    
+
 
     const onCreateTask = () => {
         //check for empty field
         const isEmpty = []
-        for(const key in task) {
-            if(!task[key]) {
+        for (const key in task) {
+            if (!task[key]) {
                 isEmpty.push(key)
             }
         }
 
-        if(isEmpty[0]) {
+        if (isEmpty[0]) {
             const msg = isEmpty.reduce((acc, val) => acc + ", " + val)
             return showMessage({
                 message: msg + " must not be empty",
@@ -58,7 +59,7 @@ function CreateTaskPage({ navigation, appState, createTask }) {
         }
 
         console.log(task)
-       
+
 
         //create task
         createTask(task, () => {
@@ -67,92 +68,93 @@ function CreateTaskPage({ navigation, appState, createTask }) {
     }
 
 
-    return(
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate("TasksPage")
-                }}>
-                    <AntDesign name="back" size={24} color="#429ef5" style={styles.icon} />
-                </TouchableOpacity>
-                <Text style={{ color: "#429ef5", marginLeft: 50, fontWeight: "bold", fontSize: 25 }} >CREATE TASK</Text>
-            </View>
-
-
-
-            <View>
-                <View>
-                    <Text>Title</Text>
-                    <TextInput
-                        placeholderTextColor="#aaaaaa"
-                        placeholder="Enter task name"
-                        value={task.title}
-                        onChangeText={(val) => onChange(val, "title")}
-                    />
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate("TasksPage")
+                    }}>
+                        <AntDesign name="back" size={24} color="#429ef5" style={styles.icon} />
+                    </TouchableOpacity>
+                    <Text style={{ color: "#429ef5", marginLeft: 50, fontWeight: "bold", fontSize: 25 }} >CREATE TASK</Text>
                 </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
+
+
 
                 <View>
-                    <Text>Description</Text>
-                    <TextInput 
-                        placeholderTextColor="#aaaaaa"
-                        placeholder="Task description should be as comprehensive"
-                        value={task.description}
-                        onChangeText={(val) => onChange(val, "description")}
-                    />
-                </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Title</Text>
+                        <TextInput
+                            placeholderTextColor="#aaaaaa"
+                            placeholder="Enter task name"
+                            value={task.title}
+                            onChangeText={(val) => onChange(val, "title")}
+                        />
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
 
-                <View>
-                    <Text>Location</Text>
-                    <TextInput 
-                        placeholderTextColor="#aaaaaa"
-                        placeholder="Enter task location"
-                        value={task.location}
-                        onChangeText={(val) => onChange(val, "location")}
-                    />
-                </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Description</Text>
+                        <TextInput
+                            placeholderTextColor="#aaaaaa"
+                            placeholder="Task description should be as comprehensive"
+                            value={task.description}
+                            onChangeText={(val) => onChange(val, "description")}
+                        />
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
 
-                <View>
-                    <Text>Date</Text>
-                    <Text onPress={() => setShowDatePicker(true)}>{task.date?.toString() || "Enter Date"}</Text>
-                    {
-                        showDatePicker && (
-                            <DateTimePicker 
-                                value={task.date || new Date()}
-                                onChange={onDateChange}
-                            />
-                        )
-                    }
-                </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Location</Text>
+                        <TextInput
+                            placeholderTextColor="#aaaaaa"
+                            placeholder="Enter task location"
+                            value={task.location}
+                            onChangeText={(val) => onChange(val, "location")}
+                        />
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
 
-                <View>
-                    <Text>Phone</Text>
-                    <TextInput 
-                        placeholderTextColor="#aaaaaa"
-                        placeholder="Enter mobile number"
-                        value={task.phone}
-                        onChangeText={(val) => onChange(val, "phone")}
-                    />
-                </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Date</Text>
+                        <Text onPress={() => setShowDatePicker(true)}>{task.date?.toString() || "Enter Date"}</Text>
+                        {
+                            showDatePicker && (
+                                <DateTimePicker
+                                    value={task.date || new Date()}
+                                    onChange={onDateChange}
+                                />
+                            )
+                        }
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
 
-                <View>
-                    <Text>Amount</Text>
-                    <TextInput 
-                        placeholderTextColor="#aaaaaa"
-                        placeholder="Set an amount"
-                        keyboardType="numeric"
-                        value={task.amount}
-                        onChangeText={(val) => onChange(val, "amount")}
-                    />
-                </View>
-                <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 20 }}></Text>
-            </View>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Phone</Text>
+                        <TextInput
+                            placeholderTextColor="#aaaaaa"
+                            placeholder="Enter mobile number"
+                            value={task.phone}
+                            onChangeText={(val) => onChange(val, "phone")}
+                        />
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
 
-            <TouchableOpacity onPress={onCreateTask}>
+                    <View>
+                        <Text style={{ marginTop: 20 }}>Amount</Text>
+                        <TextInput
+                            placeholderTextColor="#aaaaaa"
+                            placeholder="Set an amount"
+                            keyboardType="numeric"
+                            value={task.amount}
+                            onChangeText={(val) => onChange(val, "amount")}
+                        />
+                    </View>
+                    <Text style={{ backgroundColor: "#dde3ed", height: 2, width: 270, marginTop: 10 }}></Text>
+                </View>
+
+                <TouchableOpacity onPress={onCreateTask}>
                     <View style={{
                         backgroundColor: "#429ef5", width: 200, height: 45,
                         marginTop: 40,
@@ -162,23 +164,37 @@ function CreateTaskPage({ navigation, appState, createTask }) {
                         <Text style={{ color: "white", textAlign: "center", marginTop: 10 }}> Create </Text>
                     </View>
                 </TouchableOpacity>
-        </View>
+            </View>
+            </ScrollView>
 
     )
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+                container: {
+                flex: 1,
         // alignItems: "center",
+        marginLeft:20
     },
-})
+    header: {
+                // backgroundColor:"#dde3ed",
+                width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 50,
+        marginBottom:20
+    },
+    // icon:{
+                // marginTop: 50,
+                // marginLeft: 40
+                // },
+            })
 
 const mapStateToProps = (state) => {
     return {
-        appState: state
+                appState: state
     }
-} 
+}
 
 export default connect(mapStateToProps, {createTask})(CreateTaskPage)
