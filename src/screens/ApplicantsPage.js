7cdbb79dd9284de9283b0df8353a685a4ded98aa
Doreen Mensah <doreenradiance@gmail.com>
2021-04-13 11:54:16 +0000

    Changed the styling of the header

--- src/screens/ApplicantsPage.js
+++ src/screens/ApplicantsPage.js
@@ -1,77 +1,83 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, Image, TextInput, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function CreateTaskPage({ navigation }) {
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate("TasksPage")
-                }}>
-                    <AntDesign name="back" size={24} color="#429ef5" style={styles.icon} />
-                </TouchableOpacity>
-                <Text style={{ color: "#429ef5", marginLeft: 50, fontWeight: "bold", fontSize: 25 }} >APPLICANTS</Text>
-            </View>
-
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
-
-            <Text style={{ fontSize: 15, fontWeight: "bold" }}>Task Name</Text>
-            <Text style={{ fontSize: 12 }}>Description</Text>
-
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
-
-            <View>
-                <Image source={require('../../assets/john-fornander-OY03NQ4VU7E-unsplash.jpg')} style={styles.DP} />
-                <View>
-                <Text>Bruce Emmanuel</Text>
-                <Text>bruce.emma@gmail.com</Text>
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate("TasksPage")
+                    }}>
+                        <AntDesign name="back" size={24} color="#429ef5" style={styles.icon} />
+                    </TouchableOpacity>
+                    <Text style={{ color: "#429ef5", marginLeft: 70, fontWeight: "bold", fontSize: 25 }} >APPLICANTS</Text>
                 </View>
-                <Text style={{color: "#429ef5"}}>Accept</Text>
-            </View>
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
 
-            <View>
-                <Image source={require('../../assets/nolan-ayala-f_jqN8r_mCE-unsplash.jpg')} style={styles.DP} />
-                <View>
-                <Text>Felicia Sarpong</Text>
-                <Text>felicia.sarpong@gmail.com</Text>
+                
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
+
+                <View style={{alignItems:"center",marginTop:10}}>
+                    <Text style={{ fontSize: 30, fontWeight: "bold" }}>Task Name</Text>
+                    <Text style={{ fontSize: 12 }}>Description</Text>
                 </View>
-                <Text style={{color: "#429ef5"}}>Accept</Text>
-            </View>
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
 
-            <View>
-                <Image source={require('../../assets/usman-yousaf-q2q5CdLuWnI-unsplash.jpg')} style={styles.DP} />
-                <View>
-                <Text>Johnson Nanevie</Text>
-                <Text>johnson.nanevie@gmail.com</Text>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
+
+                <View style={styles.applicants}>
+                    <Image source={require('../../assets/john-fornander-OY03NQ4VU7E-unsplash.jpg')} style={styles.DP} />
+                    <View style={{ marginTop: 30, marginLeft: 10 }}>
+                        <Text>Bruce Emmanuel</Text>
+                        <Text>bruce.emma@gmail.com</Text>
+                    </View>
+                    <Text style={{ color: "#429ef5", marginTop: 35, marginLeft: 25, fontWeight: "bold" }}>Accept</Text>
                 </View>
-                <Text style={{color: "#429ef5"}}>Accept</Text>
-            </View>
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
 
-            <View>
-                <Image source={require('../../assets/DP.jpg')} style={styles.DP} />
-                <View>
-                <Text>Abigail Ofori</Text>
-                <Text>abigail.ofori.com</Text>
+                <View style={styles.applicants}>
+                    <Image source={require('../../assets/nolan-ayala-f_jqN8r_mCE-unsplash.jpg')} style={styles.DP} />
+                    <View style={{ marginTop: 30, marginLeft: 10 }}>
+                        <Text>Felicia Sarpong</Text>
+                        <Text>felicia.sarp@gmail.com</Text>
+                    </View>
+                    <Text style={{ color: "#429ef5", marginTop: 35, marginLeft: 25, fontWeight: "bold" }}>Accept</Text>
                 </View>
-                <Text style={{color: "#429ef5"}}>Accept</Text>
-            </View>
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
 
-            <View>
-                <Image source={require('../../assets/jesus-santos-ZBBKk2-Cgf8-unsplash.jpg')} style={styles.DP} />
-                <View>
-                <Text>Doreen Mensah</Text>
-                <Text>doreen.mensah@gmail.com</Text>
+                <View style={styles.applicants}>
+                    <Image source={require('../../assets/usman-yousaf-q2q5CdLuWnI-unsplash.jpg')} style={styles.DP} />
+                    <View style={{ marginTop: 30, marginLeft: 10 }}>
+                        <Text>Johnson Nanevie</Text>
+                        <Text>john.nanevie@gmail.com</Text>
+                    </View>
+                    <Text style={{ color: "#429ef5", marginTop: 35, marginLeft: 25, fontWeight: "bold" }}>Accept</Text>
                 </View>
-                <Text style={{color: "#429ef5"}}>Accept</Text>
-            </View>
-            <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 300, }}></Text>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
 
-        </View>
+                <View style={styles.applicants}>
+                    <Image source={require('../../assets/DP.jpg')} style={styles.DP} />
+                    <View style={{ marginTop: 30, marginLeft: 10 }}>
+                        <Text>Abigail Ofori</Text>
+                        <Text>abigail.ofori.com</Text>
+                    </View>
+                    <Text style={{ color: "#429ef5", marginTop: 35, marginLeft: 70, fontWeight: "bold" }}>Accept</Text>
+                </View>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
+
+                <View style={styles.applicants}>
+                    <Image source={require('../../assets/jesus-santos-ZBBKk2-Cgf8-unsplash.jpg')} style={styles.DP} />
+                    <View style={{ marginTop: 30, marginLeft: 10 }}>
+                        <Text>Doreen Mensah</Text>
+                        <Text>doreen.mensah@gmail.com</Text>
+                    </View>
+                    <Text style={{ color: "#429ef5", marginTop: 35, marginLeft: 10, fontWeight: "bold" }}>Accept</Text>
+                </View>
+                <Text style={{ backgroundColor: "#dde3ed", height: 1.5, width: 310, marginTop: 10 }}></Text>
+
+            </View>
+        </ScrollView>
     )
 }
 
@@ -79,11 +85,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // alignItems: "center",
+        marginLeft: 20
+    },
+    // icon: {
+    //     marginTop: 50,
+    //     marginLeft: 40
+    // },
+    header: {
+        // backgroundColor:"#dde3ed",
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 50,
+        marginBottom: 20
+    },
+    applicants: {
+        flexDirection: "row"
     },
     DP: {
         borderRadius: 50,
         height: 80,
         width: 80,
-        marginTop: 50
+        marginTop: 10
     },
 })
